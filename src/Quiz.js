302667b2bf13d18e1ff9c80c1ca2cd7595aa6b74
Elham <elham.khani@gmail.com>
2022-06-01 15:25:39 +0100

    create quiz

---
+++ src/Quiz.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import QuizQuestion from './QuizQuestion';
+import QuizEnd from './QuizEnd';
+
+let quizData = require('./quiz_data.json');
+
+class Quiz extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { quiz_position: 1 };
+	}
+	showNextQuestion = () => {
+		this.setState({ quiz_position: this.state.quiz_position + 1 });
+	};
+	handleResetClick = () => {
+		this.setState({ quiz_position: 1 });
+	};
+
+	render() {
+		const isQuizEnd =
+			quizData.quiz_questions.length === this.state.quiz_position - 1;
+		return (
+			<div>
+				{isQuizEnd ? (
+					<QuizEnd resetQuiz={this.handleResetClick.bind(this)} />
+				) : (
+					<QuizQuestion
+						showNextQuestionHandler={this.showNextQuestion.bind(this)}
+						quiz_question={
+							quizData.quiz_questions[this.state.quiz_position - 1]
+						}
+					/>
+				)}
+			</div>
+		);
+	}
+}
+
+export default Quiz;
---
+++ src/QuizQuestion.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import QuizQuestionButton from './QuizQuestionButton';
+
+class QuizQuestion extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { incorrectAnswer: false };
+	}
+
+	handleClick = (buttonText) => {
+		const corrcetAnswer = this.props.quiz_question.answer === buttonText;
+		this.setState({
+			incorrectAnswer: !corrcetAnswer
+		});
+		if (corrcetAnswer) this.props.showNextQuestionHandler();
+	};
+	render() {
+		return (
+			<main>
+				<section>
+					<p>{this.props.quiz_question.instruction_text}</p>
+				</section>
+				<section className="buttons">
+					<ul>
+						{this.props.quiz_question.answer_options.map(
+							(answer_option, index) => (
+								<QuizQuestionButton
+									clickHandler={this.handleClick.bind(this)}
+									key={index}
+									button_text={answer_option}
+								/>
+							)
+						)}
+					</ul>
+				</section>
+				{this.state.incorrectAnswer ? (
+					<p className="error">Sorry, that's not right.</p>
+				) : (
+					''
+				)}
+			</main>
+		);
+	}
+}
+
+export default QuizQuestion;
